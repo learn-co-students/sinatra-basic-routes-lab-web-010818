be1b31fcc1ec542bccbe7203f76725f9a3dede30
GuttermanA <alexanderf.gutterman@gmail.com>
2018-01-29 10:04:09 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Alex"
+  end
+
+  get '/hometown' do
+    "My hometown is East Norwich"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Stairway to Heaven"
+  end
 end
