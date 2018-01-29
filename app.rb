6d403f47e17b07ab8480466c0f828999610e8b51
Osama Miro <o.miro650@gmail.com>
2018-01-29 15:31:43 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Osama"
+end
+
+get '/hometown' do
+ "My hometown is yup"
+end
+
+get '/favorite-song' do
+  'My favorite song is yup'
+end
+
 end
