a70fa6dbbbd21d19fb9367049a7d2383d7f0dba6
feihafferkamp <fei.hafferkamp@gmail.com>
2018-01-29 21:57:02 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Fei."
+  end
+
+  get '/hometown' do
+     "My hometown is Omaha."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Everything by City of the Sun."
+  end
 end
